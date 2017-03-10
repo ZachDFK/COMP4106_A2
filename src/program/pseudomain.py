b30c1bc36884b064d4284ccf4b4ae6af59774217
ZachDFK <zacharie.gauthier@gmail.com>
2017-03-10 16:56:48 -0500

    init commit

---
+++ src/program/pseudomain.py
@@ -0,0 +1,27 @@
+#the main file inside the src
+
+from appJar import gui
+from .game import game_gui
+
+class Main:
+    
+    def press(self,btn):
+        type = -1 
+        if btn == "Player vs. Player":
+            type = 0
+        elif btn == "Player vs. AI":
+            type = 1
+        elif btn == "AI vs. AI":
+            type = 2
+        else:
+            self.app.stop()
+            
+        game_app = game_gui.GameApp(type)
+        
+    def __init__(self):
+        self.app = gui("Assignment 2","800x600")
+        self.app.addLabel("assignment2_title","Welcome to AI Assignment 2", 0, 0)
+        self.app.addLabel("authorFiller","Name:\tZacharie Gauthier \nStudent ID:\t100897337",1,0)
+        self.app.addButtons(["Player vs. Player","Player vs. AI", "AI vs. AI","Quit"],self.press,3,0,2)
+        self.app.go()
+
